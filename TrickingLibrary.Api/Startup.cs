ab5b9af59f41e2ab7a31ef9535756dec571f1475
Mikhail Matsiy <mmatsiy@Ascendix.net>
2020-09-16 18:36:35 +0300

    Web Api prj added

---
+++ TrickingLibrary.Api/Startup.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace TrickingLibrary.Api
+{
+	public class Startup
+	{
+		private const string AllCars = "All";
+
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddControllers();
+
+			services.AddCors(options => 
+				options.AddPolicy(AllCars, build =>
+					build.AllowAnyHeader()
+						.AllowAnyOrigin()
+						.AllowAnyMethod()));
+		}
+
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseCors(AllCars);
+
+			app.UseRouting();
+			
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapDefaultControllerRoute();
+			});
+		}
+	}
+}
